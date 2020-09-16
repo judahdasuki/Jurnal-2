7fa3ef4df002dfb63009cf47f7db3f73b8b0c630
judahdasuki <judahdasuki93@gmail.com>
2020-09-16 15:27:55 +0700

    Hello.java

---
+++ Praktikum2/Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+
+	public static void main(String[] args){
+		System.out.println("Nama : Judah Dasuki");
+		System.out.println("Kelas : D3IF-44-02");
+		System.out.println("NIM : 6706200002");
+	}
+}
